2dd21bca72f8f87c4d0dc49b7432174fa2123b17
Jypee-0318 <jaypeedcruz18@gmail.com>
2025-02-13 15:03:51 +0800

    meow

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- myproject/urls.py
+++ myproject/urls.py
@@ -4,4 +4,4 @@ from django.urls import path, include
 urlpatterns = [
 path('admin/', admin.site.urls),
 path('portfolio/', include('portfolio.urls', namespace="portfolio")),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
